f1b84892cef834e94f914b08a1334e8de877955c
Daniel Mullaney <danftv@gmail.com>
2021-01-11 01:44:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a,b) {
 function divide(a,b) {
   return a / b;
 }
-function dec(n) {
-  return n -= 1;
-}
 function inc(n) {
   return n += 1;
+}
+function dec(n) {
+  return n -= 1;
 }
